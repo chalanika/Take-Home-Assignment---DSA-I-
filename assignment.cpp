1a97abe5bd4e8431dc2f0dd85ad139094861023e
chalanika <chalanikasewwandi@gmail.com>
2019-05-16 11:51:46 +0530

    get user input2

--- assignment.cpp
+++ assignment.cpp
@@ -17,15 +17,26 @@ int main(){
 	
 	cout << "Enter no of Test cases: ";
 	cin >> T;
-	cout << "Enter no of  shelves: ";
-	cin >> N;
-	cout << "Enter no of books : ";
-	for(int i=0; i<N; i++){
-		
-		cin >> n ;
 	
+	for(int i=0; i<T; i++){
+		cout << "Enter no of  shelves: ";
+		cin >> N;
+		
+		cout << "Enter no of books : ";
+		for(int i=0; i<N; i++){
+			cin >> n ;
+		}
+		cout << "Enter no of  queries : ";
+		cin >> Q;
+		
+		cout << "Enter queries : ";
+		int arr[];
+		for(int i=0; i<Q; i++){
+			cin >> arr[i] ;
+		}
+		
+	
+		
 	}
-	cout << "Enter no of  queries : ";
-	cin >> Q;
 	
 }
