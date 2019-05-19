21589473e55f595cbb287e23a6f45a14f988522a
chalanika <chalanikasewwandi@gmail.com>
2019-05-19 13:25:56 +0530

    sort modified1

--- assignment.cpp
+++ assignment.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+void sort(int beg, int end);
 
 typedef struct node{
 	
@@ -13,8 +14,8 @@ typedef struct node{
 
 node *start = NULL;
 node *ptr = NULL;
-int T,N,n,Q;
-node*head,node*tail =NULL;
+int T,N,n,Q,beg,end;
+
 
 int main(){
 	
@@ -27,7 +28,8 @@ int main(){
 		cout << "Enter no of  shelves: ";
 		cin >> N;
 		
-		cout << "Enter no of books : ";	
+		cout << "Enter no of books : ";
+		//create node
 		for(int i=1; i<=N; i++){
 			node *shelf = new node();
 			shelf->rank = i;
@@ -45,28 +47,30 @@ int main(){
 			ptr = shelf;			
 		}
 		
+	
+		
 		cout << "Enter no of  queries : ";
 		cin >> Q;
 		
 		cout << "Enter queries : "<<endl;
-		int a,arr[4],i=0;
-		for(int j=0; j<Q; j++){
+		int a,arr[4];
+		for(int i=0; i<Q; i++){
 			
 			cin >> arr[i];
 			if(arr[i]==0){
-				for(int i=1; i<4; i++){
-					cin >> arr[i];	
+				for(int j=1; j<4; j++){
+					cin >> arr[j];	
 				}
-					head = arr[i];
-					tail = arr[i+1];
-					sort(head,tail);	
+					beg = arr[1];
+					end = arr[2];
+				
+					sort(beg,end);
+					
 			}else{
-				for(int i=1; i<3; i++){
-					cin >> arr[i];	
+				for(int j=1; j<3; j++){
+					cin >> arr[j];	
 				}
-					head = arr[i];
-					tail = arr[i+1]	;
-					sort(head,tail);
+					
 			}	
 		}
 		
@@ -74,27 +78,64 @@ int main(){
 		cout <<"---------------"<<endl;
 		
 	}
-	
+
+	node *temp = new node();
+	temp=start;
+	while(temp!=NULL){
+		cout << temp->no_of_books<< temp->rank << temp->sorted<< endl;
+		temp=temp->next;
+	}
+
 }
 
-void sort(node *head, node*tail){
+void sort(int beg, int end){
+	node *head=NULL;
+	node *tail =NULL;
 	
-	node *i,*j;
-	int temp;
+		node *p = new node();
+		p=start;
+	while(p!=NULL){
+			
+		if(beg == p->rank){
+			head = p;
+		
+		}
+		if(end == p->rank){
+			tail = p;
+		
+		}
+		p = p->next;
+	}
+
+	node *temp1 = NULL;
+	node *temp2 = NULL;
+	
+	int t;
 	head->sorted = head->no_of_books;
-	
-	for(i=head; i->next=tail->next;  i=i->next){
+	cout<< head->sorted;
+	temp1=head;
+	while( temp1->next!=tail->next){
+		cout << "vgdftsygiu";
+		temp2=temp1->next;
 		
-		j=i>next;
-		
-		while(j->prev->sorted > j->no_of_books && j->prev != NULL){
-			temp = i -> sorted;
-			i->sorted= j->no_of_books;
-			j->sorted=temp;
-			j= = j->prev;
+		while(temp2->prev->sorted > temp2->no_of_books && temp2->prev != NULL){
+			cout << "vjhyfugu";
+			t = temp1->sorted;
+			temp1->sorted= temp2->no_of_books;
+			temp2->sorted=t;
+			temp2= temp2->prev;
 			
 		}
+		temp1 = temp1->next;
 	}
+	
+	node *temp = new node();
+	temp=start;
+	while(temp!=NULL){
+		cout << temp->no_of_books<< temp->rank << temp->sorted<< endl;
+		temp=temp->next;
+	}
+
 }
 
 
