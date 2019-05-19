0e861fe25641e5e22244acdadcbdeb5ce46a29d1
chalanika <chalanikasewwandi@gmail.com>
2019-05-19 21:19:44 +0530

    sort modified finished

--- assignment.cpp
+++ assignment.cpp
@@ -16,22 +16,21 @@ void sort(int beg, int end , int find, node *start);
 
 int main(){
 	
-
-	
-
 int T,N,n,Q;
 
-	cout << "Enter no of Test cases: ";
-	cin >> T;
-	node *start = NULL;
-	
+cout << "Enter no of Test cases: ";
+cin >> T;
+node *start = NULL;
+
 	for(int i=0; i<T; i++){
 		cout << "Enter no of  shelves: ";
 		cin >> N;
+		//craete ptr node
 		node *ptr = NULL;
+		//create start node
 		node *start = NULL;
 		cout << "Enter no of books : ";
-		//create node
+		//create nodes of shelves
 		for(int i=1; i<=N; i++){
 			node *shelf = new node();
 			shelf->rank = i;
@@ -48,34 +47,26 @@ int T,N,n,Q;
 			}
 			ptr = shelf;			
 		}
-		
-	
-		
 		cout << "Enter no of  queries : ";
 		cin >> Q;
-		
 		cout << "Enter queries : "<<endl;
 		int a,arr[4];
 		for(int i=0; i<Q; i++){
-			
 			cin >> arr[0];
 			if(arr[0]==0){
 				for(int j=1; j<4; j++){
 					cin >> arr[j];	
 				}
-				
-				sort(arr[1],arr[2],arr[3],start);
-				
-					
+				sort(arr[1],arr[2],arr[3],start);		
 			}else{
 				for(int j=1; j<3; j++){
 					cin >> arr[j];	
 				}
+				//update no of books
 				node *t = start;
 					while(t!=NULL){
 					if(arr[1]== t->rank){
 						t->no_of_books=arr[2];
-						cout<< t->no_of_books<< t->rank<<endl;
 						break;
 					}
 					t=t->next;
@@ -83,49 +74,29 @@ int T,N,n,Q;
 					
 			}	
 		}
-		
-		
 		cout <<"---------------"<<endl;
-		
 	}
-
-	
-
 }
 
 void sort(int beg, int end, int find , node *start){
 	
 	node *head=NULL;
 	node *tail =NULL;
-	
-	
-	
-	node *p = new node();
-	p=start;
+	node *p = start;
 	while(p!=NULL){
-			
 		if(beg == p->rank){
 			head = p;
-		
 		}
 		if(end == p->rank){
 			tail = p;
-		
 		}
 		p = p->next;
 	}
-	node *x = new node();
-	x=start;
+	node *x = start;
 	while(x!=NULL){
 		x->sorted = x->no_of_books;
-	
 		x=x->next;
 	}
-	
-	
-
-	
-
 	node *temp1 = NULL;
 	node *temp2 = NULL;
 	
@@ -137,17 +108,13 @@ void sort(int beg, int end, int find , node *start){
 	while( temp1 !=tail){
 	temp2=temp1->next;
 	while(temp2->rank > head->rank ){
-		
 		if(temp2->prev->sorted >= temp2->sorted){
-		
 			t = temp2->prev->sorted;
 			temp2->prev->sorted= temp2->sorted;
 			temp2->sorted=t;
-		
 		}
 		temp2= temp2->prev;	
-	}
-		
+	}	
 	temp1 = temp1->next;
 	
 		
